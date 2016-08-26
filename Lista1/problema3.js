92b9e0a2b7d0e8073b0081665c5cc95b9a47c3d2
Rayana Alencar <rvsa2@cin.ufpe.br>
2016-08-26 08:26:04 -0300

    Add files via upload

--- Lista1/problema3.js
+++ Lista1/problema3.js
@@ -12,11 +12,20 @@ function returnAttValues(arrayentrada,atributo1,atributo2){
 	return arrayentrada.map(attValue(atributo1,atributo2));
 }
 
-function render(data,atributo1,atributo2) {
+function render(data,atributo1,atributo2,xmin,xmax,ymin,ymax) {
 	console.log("aquiiii");
+	console.log(xmin," ",xmax);
 	var pontos = d3.select("body").select("svg").select("g").selectAll("circle").data(data);
 	pontos.exit().remove();
-	pontos.enter().append("circle").attr("cx",function(d){ return d[atributo1];}).attr("cy",function(d){ return d[atributo2];}).attr("r",50).attr("sytle","fill:rgb(204,102,0);stroke:black;stroke-width:8");
+	pontos.enter().append("circle").attr("cx", function (d,xmin,xmax){
+												console.log(d," ",xmin,xmax);
+												 return (d[atributo2]-xmin/xmax-xmin);
+												 }
+										).attr("cy", function (ymin,ymax){
+												return function(d){ 
+												 return (d[atributo1]-ymin/ymax-ymin);
+												 }
+											}).attr("r",50).attr("sytle","fill:rgb(204,102,0);stroke:black;stroke-width:8");
 }
 
 
@@ -25,19 +34,17 @@ function calcPosition(data,especie,atributo1){
 
 
 }
-function getMinimum (arrayentrada,especie,coluna){
-	var onlyEspecie = returnSpecie(arrayentrada,especie);
-	var saida =  onlyEspecie.reduce(minn(coluna));
+function getMinimum (arrayentrada,coluna){
+	var saida =  arrayentrada.reduce(minn(coluna));
 	saida = saida[coluna];
-	//console.log("O minimo valor do atributo ",coluna," da especie ",especie, " eh ",saida);
+	console.log("O minimo valor do atributo ",coluna, " eh ",saida);
 	return saida;
 }
 
-function getMax (arrayentrada,especie,coluna){
-	var onlyEspecie = returnSpecie(arrayentrada,especie);
-	var saida =  onlyEspecie.reduce(max(coluna));
+function getMax (arrayentrada,coluna){
+	var saida =  arrayentrada.reduce(max(coluna));
 	saida = saida[coluna];
-	//console.log("O maximo valor do atributo ",coluna," da especie ",especie, " eh ",saida);
+	console.log("O maximo valor do atributo ",coluna, " eh ",saida);
 	return saida;
 }
 
@@ -75,7 +82,11 @@ function max(atributo){
 function main(arrayentrada,atributo1,atributo2){
 	var att1 = returnAttValues(arrayentrada,atributo1,atributo2);
 	console.log("array um eh ",att1);
+	var xMaximo = getMax(att1,atributo2);
+	var xMin = getMinimum(att1,atributo2);
+	var yMaximo = getMax(att1,atributo1);
+	var yMin = getMinimum(att1,atributo1);
 	d3.select("body").select("svg").select("g").append("line").attr("x1",0).attr("y1",0).attr("x2",0).attr("y2",700).attr("style","fill:none;stroke:black;stroke-width:8");
 	d3.select("body").select("svg").select("g").append("line").attr("x1",0).attr("y1",0).attr("x2",700).attr("y2",0).attr("style","fill:none;stroke:black;stroke-width:8");
-	render(att1,atributo1,atributo2);
+	render(att1,atributo1,atributo2,xMin,xMaximo,yMin,yMaximo);
 }
