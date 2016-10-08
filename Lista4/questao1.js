fc9105189c267381e5ddc522a584b459a7f14367
Rayana Alencar <rvsa2@cin.ufpe.br>
2016-10-08 11:43:07 -0300

    questao1 pronta

--- Lista4/questao1.js
+++ Lista4/questao1.js
@@ -1,43 +1,47 @@
-var margin = {top: 10, right: 20, bottom: 10, left: 20};
-var w = 1000 - margin.left - margin.right;
-var h = 1000 - margin.top - margin.bottom;
-var meses = ["Janeiro","Fevereiro","Marco","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"];
-var svg = 0;
-var xAxis = 0;
-var yAxis =0;
-var xScale = 0;
-var yScale = 0;
-var svg = 0;
-var ytickers = new Set();
+	var margin = {top: 20, right: 20, bottom: 20, left: 20};
+	var w = 900 - margin.left - margin.right;
+	var h = 900 - margin.top - margin.bottom;
+	var meses = ["Janeiro","Fevereiro","Marco","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"];
+	var svg = 0;
+	var xAxis = 0;
+	var yAxis =0;
+	var xScale = 0;
+	var yScale = 0;
+	var svg = 0;
+	var ytickers = new Set();
 
 function init(data) {
 
-console.log("Data ",data);
+	console.log("Data ",data);
 
-var max = -151515151515150;
-var min = 5589624451155840;
-var array =[];
-for(var i =1;i<13;i++){
-	array = data[i]
-	for (var j =0;j<array.length;j++){
-		if(max<array[j]){
-			max = array[j];
-		}
+	var max = -151515151515150;
+	var min = 5589624451155840;
+	var array =[];
+	for(var i =1;i<13;i++){
+		array = data[i]
+		for (var j =0;j<array.length;j++){
+			if(max<array[j]){
+				max = array[j];
+			}
 
-		if(min>data[i][j]){
-			min = array[j];
+			if(min>data[i][j]){
+				min = array[j];
+			}
 		}
 	}
-}
 
-var xScale = d3.scaleBand()
-				.domain(d3.range(12))
-				.rangeRound([0, w])
-				.padding(3.5);
-    
-var yScale = d3.scaleLinear()
-        .domain([min,max])
-		.range([h,0]);
+	var xScale = d3.scaleBand()
+					.domain(d3.range(12))
+					.rangeRound([0, w])
+					.padding(3.5);
+	
+	var minD = min;
+	if(minD>0){
+		minD = 0;
+	}
+	var yScale = d3.scaleLinear()
+	        .domain([minD,max+5])
+			.range([h,0]);
 
 
   	svg = d3.select("body")
@@ -46,87 +50,91 @@ var yScale = d3.scaleLinear()
 			.attr("height", h+margin.bottom + margin.top)
 			.attr("transform","translate(" + margin.left + "," + margin.top + ")");
 
-svg.append("g").attr("id","xAxis")
-				.attr("transform","translate(" + 0 +"," + h + ")");
+	svg.append("g").attr("id","xAxis")
+					.attr("transform","translate(" + 0 +"," + yScale(0)+ ")");
 
-svg.append("g").attr("id","yAxis").attr("transform","translate(" + (margin.left) + ",0)");
+	svg.append("g").attr("id","yAxis").attr("transform","translate(" + (margin.left) + ",0)");
 
- var xAxis = d3.axisBottom(xScale).tickFormat(function(d) {return meses[d]; });	
+	var xAxis = d3.axisBottom(xScale).tickFormat(function(d) {return meses[d]; });	
 
- var xAxisGroup = d3.select("#xAxis")
-				.transition()
-				.call(xAxis);
+	var xAxisGroup = d3.select("#xAxis")
+	.transition()
+	.call(xAxis);
 
- var yAxis = d3.axisLeft(yScale).ticks(24);		
+	var yAxis = d3.axisLeft(yScale).ticks(24);		
 
-  var yAxisGroup = d3.select("#yAxis")
-  					.transition()
-  					.call(yAxis);	
-
+	var yAxisGroup = d3.select("#yAxis")
+		.transition()
+		.call(yAxis);	
   
 
   	for(var i =1;i<=12;i++){
   		console.log(meses[i-1],":");
   		boxPlot(data[i],min,max,i-1);
   	}
-	
+	/*
 	var labels = Array.from(ytickers);
 
 	labels = labels.sort(function(a, b) {
 				  return a - b;
 				});
+ 	var yAxis = d3.axisLeft(yScale).tick(24);	
 
- var yAxis = d3.axisLeft(yScale).tickValues(labels);	
-
-  var yAxisGroup = d3.select("#yAxis")
+  	var yAxisGroup = d3.select("#yAxis")
   					.transition()
-  					.call(yAxis);	
+  					.call(yAxis);	*/
 }
 
 
 function boxPlot(data,minE,maxE,indice){
 
-var data_length = data.length;
+	var data_length = data.length;
 
-data = data.sort(function(a, b) {
-  return a - b;
-});
+	data = data.sort(function(a, b) {
+	return a - b;
+	});
 
-var min = data.reduce(minCallback);
-console.log("minimum ",min);
-var max = data.reduce(maxCallback);
+	var min = data.reduce(minCallback);
+	console.log("minimum ",min);
+	var max = data.reduce(maxCallback);
+
+	if(max === 0 && min ===0){
+		var firstQuartile=0;
+		var secondQuartile=0;
+		var thirdQuartile =0;
+	}else {
+
+		var k = 1*(data_length +1) / 4; 
+		k = Math.floor(k);
+		var firstQuartile = data[k-1] + (((data_length+1)/4) - k) * (data[k]-data[k-1]);
+		console.log("first quartile ",firstQuartile);
+		k = 2*(data_length +1) / 4; 
+		k = Math.floor(k); 
+		var secondQuartile =  data[k-1] + ((2*(data_length+1)/4) - k) * (data[k]-data[k-1]);
+		console.log("second quartile ",secondQuartile);
+		k = 3*(data_length +1) / 4; 
+		k = Math.floor(k); 
+		var thirdQuartile =  data[k-1] + ((3*(data_length+1)/4) - k) * (data[k]-data[k-1]);
+		console.log("third quartile ",thirdQuartile);
+	}
+
+	console.log("maximum ",max);
 
 
-var k = 1*(data_length +1) / 4; 
-k = Math.floor(k);
-var firstQuartile = data[k-1] + (((data_length+1)/4) - k) * (data[k]-data[k-1]);
-console.log("first quartile ",firstQuartile);
-k = 2*(data_length +1) / 4; 
-k = Math.floor(k); 
-var secondQuartile =  data[k-1] + ((2*(data_length+1)/4) - k) * (data[k]-data[k-1]);
-console.log("second quartile ",secondQuartile);
-k = 3*(data_length +1) / 4; 
-k = Math.floor(k); 
-var thirdQuartile =  data[k-1] + ((3*(data_length+1)/4) - k) * (data[k]-data[k-1]);
-console.log("third quartile ",thirdQuartile);
-
-console.log("maximum ",max);
-
-
-var xScale = d3.scaleBand()
+	var xScale = d3.scaleBand()
 				.domain(d3.range(12))
 				.rangeRound([0, w])
 				.padding(3.5);
-    
-var yScale = d3.scaleLinear()
-        .domain([minE,maxE])
+
+	var yScale = d3.scaleLinear()
+	    .domain([minE,maxE+5])
 		.range([h,0]);
 
 
 
-var aux = xScale(indice); 
+	var aux = xScale(indice); 
 
-var line = svg.append("line")
+	var line = svg.append("line")
 			.attr("x1",xScale(indice))
 			.attr("y1",yScale(min))
 			.attr("x2",xScale(indice))
@@ -134,7 +142,7 @@ var line = svg.append("line")
 			.attr("stroke","black")
 			.attr("stroke-width","1.5");
 			
-var lineSup = svg.append("line")
+	var lineSup = svg.append("line")
 			.attr("x1",function(d){
 				return aux-25;
 			})
@@ -146,7 +154,7 @@ var lineSup = svg.append("line")
 			.attr("stroke","black")
 			.attr("stroke-width","1.5");
 
-var lineInf = svg.append("line")
+	var lineInf = svg.append("line")
 			.attr("x1",function(d){
 				return aux-25;
 			})
@@ -159,19 +167,19 @@ var lineInf = svg.append("line")
 			.attr("stroke","black")
 			.attr("stroke-width","1.5");
 
-var rect = svg.append("rect")
+	var rect = svg.append("rect")
 			.attr("x",function(d){
 				return aux-25;
 			})
 			.attr("y",yScale(thirdQuartile))
 			.attr("height",yScale(firstQuartile)-yScale(thirdQuartile))
 			.attr("width",50)
-			.attr("fill","black")
-			.attr("opacity","0.5")
+			.attr("fill","none")
+			//.attr("opacity","0.2")
 			.attr("stroke","black");
 
 		
-var line_med = svg.append("line")
+	var line_med = svg.append("line")
 			.attr("x1",function(d){
 				return aux-25;
 			})
@@ -185,7 +193,7 @@ var line_med = svg.append("line")
 
 	ytickers.add(min);
 	ytickers.add(max);
-/*	ytickers.push(firstQuartile);
+	/*	ytickers.push(firstQuartile);
 	ytickers.push(secondQuartile);
 	ytickers.push(thirdQuartile);*/
 
@@ -208,6 +216,14 @@ var Meses = {};
 
 function entrada(ano,tipo){
 
+	if(tipo=="max"){
+		tipo = "MaxTempF";
+	}else if(tipo=="med"){
+		tipo = "MeanTempF";
+	}else{
+		tipo = "MinTempF";
+	}
+
 	for(var i =0;i<nbWeather.length;i++){
 		if(nbWeather[i].Year ===2015){
 			var j = (nbWeather[i].Month);
@@ -215,11 +231,17 @@ function entrada(ano,tipo){
 		}
 	
 	}
+
+	for (var j =1;j<=12;j++){
+		if(Meses[j].length===0){
+			Meses[j].push(0);
+		}
+	}
 	init(Meses);
 
 	//boxPlot(Meses);
 }
 
-var minCallback = ( pre, cur ) => Math.min( pre, cur );
-var maxCallback = ( pre, cur ) => Math.max( pre, cur );
+	var minCallback = ( pre, cur ) => Math.min( pre, cur );
+	var maxCallback = ( pre, cur ) => Math.max( pre, cur );
 	
